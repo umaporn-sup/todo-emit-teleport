1061e40901663358d265fc9671b867b0f3956893
umaporn-sup <umaporn@sit.kmutt.ac.th>
2024-03-12 20:30:07 +0700

    [add] git repo

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import TodoManager from './components/TodoManager.vue'
+</script>
+
+<template>
+  <div class="w-full">
+    <TodoManager />
+  </div>
+</template>
+
+<style scoped></style>
